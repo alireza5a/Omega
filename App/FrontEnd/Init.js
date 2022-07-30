f6b8429da24e8037d9011dfaa9a3400955f3a5aa
alireza5a <saeedi.mail@gmail.com>
2022-07-30 12:59:28 +0430

    Get SENTRY_DSN from Cookie with name

--- App/FrontEnd/Init.js
+++ App/FrontEnd/Init.js
@@ -1,6 +1,11 @@
-﻿Sentry.init({
+﻿function getCookie(name) {
+    var value = `; ${document.cookie}`;
+    var parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+Sentry.init({
     environment: "Testing",
-    dsn: document.cookie.substr(11),
+    dsn: getCookie('SENTRY_DSN'),
     tracesSampleRate: 1.0,
     integrations: [new Sentry.Integrations.BrowserTracing()],
     sampleRate: 1.0,
